6a3b00faa523d57465b68caa3b668361a5c92999
yousef ahmed <yousef.amer5453@gmail.com>
2022-05-25 19:01:02 +0200

    ver2

---
+++ api-nodejs/model/Coach.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose') ;
+
+const userSchema = mongoose.Schema({
+    
+    id:{
+        type: Number ,
+        require: true
+    },
+    name:{
+        type: String,
+        require: true,
+        min : 6,
+        max:255
+    },
+    email:{
+        type: String,
+        require: true,
+        max : 255,
+        min:6 
+    },
+    password:{
+        type:String,
+        require: true,
+        max: 1024,
+        min:6
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    //coachAdmin 
+    isAdmin:{
+        type:Boolean,
+        require:true
+    }
+
+});
+
+module.exports = mongoose.model( 'Coach' , userSchema ) ;
