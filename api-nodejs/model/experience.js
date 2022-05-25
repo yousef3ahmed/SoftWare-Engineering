be9a2095e3aa44c498ffe4d115a96590bad8ec3f
MennaDawood <91068763+MennaDawood@users.noreply.github.com>
2022-05-25 19:07:54 +0200

    Add Search API

---
+++ api-nodejs/model/experience.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+
+const ExperienceSchema=  mongoose.Schema({
+    name:{
+        type: String,
+        require: true,
+        min : 6,
+        max:255
+    },
+    by:{
+        type: String,
+        require: true,
+        max : 255,
+        min:6 
+    }
+   });
+
+module.exports= mongoose.model("Experience",ExperienceSchema);
